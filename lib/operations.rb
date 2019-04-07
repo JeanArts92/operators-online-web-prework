61bdd112cb09b0a100ba12c9dd5facc089d9d1f9
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 17:20:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,12 @@
-def unsafe?(speed)
-  if speed < 40
-    elsif speed > 60
-  else speed == 50
-end
+def unsafe?(speed = 55)
+if speed == speed < 40 && > 60
+  
+elsif speed == speed > 60
+  
+else speed == speed < 40
+  
+  end
+
 
 
 
